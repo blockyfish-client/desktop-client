b7b9349f7fcf32e4d13951f49e10a18609ff1915
pi <piisthebestthingintheuniverse@gmail.com>
2022-08-05 09:07:27 -0700

    changed text from download to install in updater

--- index.js
+++ index.js
@@ -324,7 +324,7 @@ const createWindow = () => {
             const updaterMain = document.getElementById("updater-main")
             const updaterBox = document.createElement("div")
             updaterMain.appendChild(updaterBox)
-            updaterBox.outerHTML = '<div style="display:none" id="update-available"><p id="update-available-text">Update available</p><div id="updater-download" class="button updater-button updater-blue">Download</div></div><p id="updater-text">No updates available</p><svg id="updater-icon" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#374151" class="bi bi-arrow-clockwise" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"></path><path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"></path></svg>'
+            updaterBox.outerHTML = '<div style="display:none" id="update-available"><p id="update-available-text">Update available</p><div id="updater-download" class="button updater-button updater-blue">Install</div></div><p id="updater-text">No updates available</p><svg id="updater-icon" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#374151" class="bi bi-arrow-clockwise" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"></path><path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"></path></svg>'
             const updateButton = document.getElementById("updater-load")
             const updateImg = document.getElementById("updater-icon")
             const updateText = document.getElementById("updater-text")
