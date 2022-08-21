20f2c05a162dd02895923b347b27a0d311f88935
pi <piisthebestthingintheuniverse@gmail.com>
2022-08-21 10:51:13 -0700

    v1.5

--- installers/windows/createinstaller.js
+++ installers/windows/createinstaller.js
@@ -1,6 +1,6 @@
 const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller
 const path = require('path')
-const version = '1.4.1'
+const version = '1.5.0'
 
 getInstallerConfig()
   .then(createWindowsInstaller)
