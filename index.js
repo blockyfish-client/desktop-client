6e1a8ec6a9a48cf5b7c92c549f5a63914788389c
pi <90981829+ThePiGuy3141@users.noreply.github.com>
2022-11-10 10:01:50 -0800

    noob vision plugin

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const os = require("os");
 const fetch = require("node-fetch");
 
 // debug mode
-const debug = true;
+const debug = false;
 
 // if (!debug) {
 process.on("uncaughtException", () => {
@@ -1853,7 +1853,7 @@ app.whenReady().then(async function makeNewWindow() {
                             ctrl_overlay.outerHTML = '<div id="ctrl-overlay" style="width: 100%;height: 100%;position: absolute;display: block;z-index:10000;pointer-events:none;"></div>'
                             var aim_overlay = document.createElement('hr')
                             document.querySelector('div.game').insertBefore(aim_overlay, document.querySelector('div.game').children[1])
-                            aim_overlay.outerHTML = '<hr id="aim-overlay" style="border: 2px #fff dotted;border-image: linear-gradient(to right, #fff8, #fff0) 1;transform-origin: left;position: absolute;top: 50%;left: 50%;width: 40vw;display:none;pointer-events:none;">'
+                            aim_overlay.outerHTML = '<hr id="aim-overlay" style="border: 1px #fff dotted;border-image: linear-gradient(to right, #f32d, #f320) 1;transform-origin: left;position: absolute;top: 50%;left: 50%;width: 40vw;display:none;pointer-events:none;">'
                             `);
 
 								//fish levels:
