7793626bf9cda7af832feb11b66851ab61e2406d
Pi <piisthebestthingintheuniverse@gmail.com>
2023-09-23 12:20:24 -0700

    delete everything

--- plugins/showGhosts.js
+++
@@ -1,8 +0,0 @@
-var name = "I see dead fish"
-var description = "Show all ghosts. This is the only way to see ghosts without becoming a ghost. Saying \\\"I see dead fish\\\" no longer works."
-var script = `
-game.currentScene.viewingGhosts = true
-`;
-exports.script = script;
-exports.name = name;
-exports.description = description;
