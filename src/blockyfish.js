aad4689e7ac2564fe2893558e42b833c639f3fc6
Pi <piisthebestthingintheuniverse@gmail.com>
2023-12-19 21:37:44 -0800

    gif pfp upload patch

--- src/blockyfish.js
+++ src/blockyfish.js
@@ -1,5 +1,3 @@
-// Game object script injector
-// Made by TheJ, aka noam
 const old = Function.prototype.bind;
 let game;
 const bind = function (...args) {
@@ -9,7 +7,25 @@ const bind = function (...args) {
 	if (Function.prototype.bind == old) {
 		return old.apply(this, args);
 	} else if (args[0] && Object.prototype.hasOwnProperty.call(args[0], "currentScene")) {
+		// Game object script injector
+		// Made by TheJ, aka noam
 		console.log("%c[TheJ Injector] Logged game object.", "color: #ff6969; font-size:125%");
+		game = args[0];
+		window.game = game;
+	} else if (args[0] && Object.prototype.hasOwnProperty.call(args[0], "prepareUpload")) {
+		// GIF pfp upload patch injector
+		// Made by Pi
+
+		// console log throttling
+		if (Date.now() - (window.pi_igpup_llts || 0) > 1000) {
+			window.pi_igpup_llts = Date.now();
+			console.log("%c[Pi Injector] Injected GIF pfp uploader patch.", "color: #e9c2ff; font-size:125%");
+		}
+		var opu = args[0].prepareUpload;
+		args[0].prepareUpload = function () {
+			args[0].createImgUrl = args[0].sourceImgUrl;
+			return opu.apply(this);
+		};
 		game = args[0];
 		window.game = game;
 	}
@@ -21,21 +37,24 @@ class Blockyfish {
 	constructor(window) {
 		this.window = window;
 		this.events = {
-			first_game_load: new Event("first-game-load", { bubbles: true, cancelable: false }),
-			game_load: new Event("game-load", { bubbles: true, cancelable: false }),
-			death: new Event("death", { bubbles: true, cancelable: false }),
-			settings_open: new Event("settings-open", { bubbles: true, cancelable: false }),
-			forums_open: new Event("forums-open", { bubbles: true, cancelable: false })
+			"first-game-load": new Event("first-game-load", { bubbles: true, cancelable: false }),
+			"game-load": new Event("game-load", { bubbles: true, cancelable: false }),
+			"death": new Event("death", { bubbles: true, cancelable: false }),
+			"settings-open": new Event("settings-open", { bubbles: true, cancelable: false }),
+			"forums-open": new Event("forums-open", { bubbles: true, cancelable: false })
 		};
 	}
 	emit(event) {
-		dispatchEvent(event);
+		if (!this.events[event]) return false;
+		dispatchEvent(this.events[event]);
 	}
 	addEventListener(event, callback) {
-		addEventListener(event, callback);
+		if (!this.events[event] || !callback) return false;
+		addEventListener(this.events[event], callback);
 	}
 	removeEventListener(event, callback) {
-		removeEventListener(event, callback);
+		if (!this.events[event] || !callback) return false;
+		removeEventListener(this.events[event], callback);
 	}
 }
 
