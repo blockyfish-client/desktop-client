74a612ada1d3ad978ff6ce97e0c6e13e6e7ede9a
Pi <piisthebestthingintheuniverse@gmail.com>
2023-12-19 21:38:02 -0800

    change event stuff

--- src/preload/events.js
+++ src/preload/events.js
@@ -2,12 +2,15 @@ let scriptExecuted = false;
 document.querySelector("button.play").addEventListener("click", () => {
 	console.log("Play button clicked");
 	if (!document.querySelector(".playing")) scriptExecuted = false;
-	if (scriptExecuted) return;
 	const openObserver = new MutationObserver(() => {
 		if (document.contains(document.querySelector(".playing"))) {
 			openObserver.disconnect();
 			scriptExecuted = true;
-			window.blockyfish.emit(window.blockyfish.events.game_load);
+			if (scriptExecuted) {
+				window.blockyfish.emit("game-load");
+			} else {
+				window.blockyfish.emit("first-game-load");
+			}
 		}
 	});
 	openObserver.observe(document.getElementById("app"), {
