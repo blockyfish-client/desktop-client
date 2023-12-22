1f1297caa34d48000836e5c2e2266e526c25cbf2
Pi <piisthebestthingintheuniverse@gmail.com>
2023-12-21 16:10:57 -0800

    death and settings-open event

--- src/preload/events.js
+++ src/preload/events.js
@@ -1,20 +1,37 @@
 window.bfe = {};
+
+// GAME UI LISTENERS
 // game-load
 // first-game-load
+// death
 window.bfe.firstLoad = true;
 document.querySelector("button.play").addEventListener("click", () => {
 	window.blockyfish.emit("play-button-click");
 	if (document.querySelector(".playing")) return;
 	console.log("Play button clicked");
-	const openObserver = new MutationObserver(() => {
+	const playObserver = new MutationObserver(() => {
 		if (document.querySelector(".playing")) {
-			openObserver.disconnect();
+			playObserver.disconnect();
 			if (window.bfe.firstLoad) {
 				window.blockyfish.emit("first-game-load");
 			} else {
 				window.blockyfish.emit("game-load");
 			}
 			window.bfe.firstLoad = false;
+
+			// death event
+			const deathScreenObserver = new MutationObserver(() => {
+				if (document.querySelector(".death-screen-container")) {
+					deathScreenObserver.disconnect();
+					window.blockyfish.emit("death");
+				}
+			});
+			deathScreenObserver.observe(document.getElementById("app"), {
+				attributes: false,
+				childList: true,
+				characterData: false,
+				subtree: true
+			});
 		}
 	});
 	openObserver.observe(document.getElementById("app"), {
@@ -24,3 +41,26 @@ document.querySelector("button.play").addEventListener("click", () => {
 		subtree: true
 	});
 });
+
+// MODAL LISTENERS
+// settings-open/close
+(() => {
+	var settingsOpen = false;
+	const modalObserver = new MutationObserver(() => {
+		// settings
+		if (document.querySelector(".modal-content #pane-0 ~ #pane-1 ~ #pane-2")) {
+			if (!settingsOpen) {
+				settingsOpen = true;
+				window.blockyfish.emit("settings-open");
+			}
+		} else {
+			settingsOpen = false;
+		}
+	});
+	modalObserver.observe(document.getElementById("app"), {
+		attributes: false,
+		childList: true,
+		characterData: false,
+		subtree: true
+	});
+})();
