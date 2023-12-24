cc3de1b386acf3e14cd17a77a29034ecc8edb60d
Pi <piisthebestthingintheuniverse@gmail.com>
2023-12-23 20:35:10 -0800

    add delete button

--- src/preload/plugins.js
+++ src/preload/plugins.js
@@ -186,7 +186,10 @@ button.small-x {
 	background: #409eff !important;
 }
 .plugin-action-button.update:hover {
-	background: #d97706 !important;
+	background: #059669 !important;
+}
+.plugin-action-button.delete:hover {
+	background: #dc2626 !important;
 }
 `;
 
@@ -205,6 +208,12 @@ function createPluginBox(name, description, author, version, hasSettings, online
 	</svg>
 </button>`;
 	const sw = `${updatable ? ub : ""}
+<button id="${name.split(" ").join("_").toLowerCase() + "_delete_button"}" class="plugin-action-button delete">
+	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+		<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+		<path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+	</svg>
+</button>
 	<div class="switch" style="margin-left: 0.5rem;">
 		<input
 			type="checkbox"
